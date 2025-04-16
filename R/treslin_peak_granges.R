11449ad101ace7b53ea9dd308982930a150f2332
Chris Sansam <63733772+sansamcl@users.noreply.github.com>
2025-04-16 11:22:31 -0500

    added peak data

---
+++ R/treslin_peak_granges.R
@@ -0,0 +1,11 @@
+#' GenomicRanges of TRESLIN Cut&Run Peaks
+#'
+#' A list of GenomicRanges representing TRESLIN peaks used in Figure 1 of Noble et al. (2024) https://doi.org/10.1101/2024.02.02.578516.
+#'
+#' All peaks are called with MACS2 and used to derive reproducible peaks in the analysis.
+#'
+#' @format A named list of GenomicRanges objects.
+#' @source GEO: GSE294620, GSE274463, GSE274464, GSE274465
+#' @usage data(treslin_peak_granges)
+#' @keywords datasets
+"treslin_peak_granges"
